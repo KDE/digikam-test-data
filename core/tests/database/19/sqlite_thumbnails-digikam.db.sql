c0701f1114bbbc34c82144f6e9a5e034d33d45a3
Gilles Caulier <caulier.gilles@gmail.com>
2022-10-14 07:58:51 +0200

    append new mime type to LFS manager

--- core/tests/database/19/sqlite_thumbnails-digikam.db.sql
+++ core/tests/database/19/sqlite_thumbnails-digikam.db.sql
@@ -1,34 +1,3 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-CREATE TABLE Thumbnails
-                            (id INTEGER PRIMARY KEY,
-                            type INTEGER,
-                            modificationDate DATETIME,
-                            orientationHint INTEGER,
-                            data BLOB);
-CREATE TABLE UniqueHashes
-                            (uniqueHash TEXT,
-                            fileSize INTEGER,
-                            thumbId INTEGER,
-                            UNIQUE(uniqueHash, fileSize));
-CREATE TABLE FilePaths
-                            (path TEXT,
-                            thumbId INTEGER,
-                            UNIQUE(path));
-CREATE TABLE Settings
-                            (keyword TEXT NOT NULL UNIQUE,
-                            value TEXT);
-CREATE TABLE CustomIdentifiers
-                        (identifier TEXT,
-                        thumbId INTEGER,
-                        UNIQUE(identifier));
-CREATE INDEX id_uniqueHashes ON UniqueHashes (thumbId);
-CREATE INDEX id_filePaths ON FilePaths (thumbId);
-CREATE INDEX id_customIdentifiers ON CustomIdentifiers (thumbId);
-CREATE TRIGGER delete_thumbnails DELETE ON Thumbnails
-                            BEGIN
-                            DELETE FROM UniqueHashes WHERE UniqueHashes.thumbId = OLD.id;
-                            DELETE FROM FilePaths WHERE FilePaths.thumbId = OLD.id;
-                            DELETE FROM CustomIdentifiers WHERE CustomIdentifiers.thumbId = OLD.id;
-                            END;
-COMMIT;
+version https://git-lfs.github.com/spec/v1
+oid sha256:171543b896d42d7789ed22ef0a1367f62b093884b54ab50c56a1ad3cf4ca6d2e
+size 1540
